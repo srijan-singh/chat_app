454b5439a39d73f0ab514cb528d0e7ac388ab970
Srijan Singh <Srijan.Singh1@ibm.com>
2024-02-06 12:19:29 +0530

    Chat App

---
+++ src/main/java/weekend/project/chat_app/ChatAppApplication.java
@@ -0,0 +1,13 @@
+package weekend.project.chat_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppApplication.class, args);
+	}
+
+}
