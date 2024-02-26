d8e966a9f87031d9cf655aa2df2bc0c241716d9e
Srijan Singh <Srijan.Singh1@ibm.com>
2024-02-26 11:11:07 +0530

    Git Properties Maven

--- src/main/java/weekend/project/chat_app/ChatAppApplication.java
+++ src/main/java/weekend/project/chat_app/ChatAppApplication.java
@@ -2,6 +2,9 @@ package weekend.project.chat_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class ChatAppApplication {
@@ -10,4 +13,13 @@ public class ChatAppApplication {
 		SpringApplication.run(ChatAppApplication.class, args);
 	}
 
+	@Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer propsConfig 
+          = new PropertySourcesPlaceholderConfigurer();
+        propsConfig.setLocation(new ClassPathResource("git.properties"));
+        propsConfig.setIgnoreResourceNotFound(true);
+        propsConfig.setIgnoreUnresolvablePlaceholders(true);
+        return propsConfig;
+    }
 }
